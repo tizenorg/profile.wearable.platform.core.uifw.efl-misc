d23ee0466dd5e440fda76f856fa9d31429832ca9
Daniel Juyung Seo <juyung.seo@samsung.com>
2015-03-03 18:44:17 +0900

    efl.sh: Categorize contents and remove unavailable variable.
    Change-Id: I0c22c4d9830b3a64a530d46995e3d1d3583fd28d
    Signed-off-by: Daniel Juyung Seo <juyung.seo@samsung.com>

--- etc/profile.d/efl.sh
+++ etc/profile.d/efl.sh
@@ -1,6 +1,10 @@
+## Elementary
 export ELM_MODULES="ctxpopup_copypasteUI>entry/api:datetime_input_ctxpopup>datetime/api"
 export ELM_MODULES="$ELM_MODULES:prefs>prefs_iface"
+
+## Ecore
 export ECORE_IMF_INPUT_PANEL_ENABLED=1
-export MULTISENSE_SND_PLAYER="tizen_snd_player"
+
+## Evas
 export EVAS_FONT_DPI=72
 export EVAS_GL_NO_BLACKLIST=1
